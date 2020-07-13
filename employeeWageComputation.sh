d4ff2c214c6d1ae4dba986859ef47977023de430
TalatiHardik <hardikhtalati@gmail.com>
2020-07-13 11:23:25 +0530

    UC1 completed

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,4 +1,14 @@
 
 #!/bin/bash -x
 
-echo "Welcome to master branch"
+echo "Welcome to UC1 Attendance Check"
+ispresent=1
+randomcheck=$(( RANDOM%2 ))
+
+if [ $ispresent -eq $randomcheck ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+
+fi

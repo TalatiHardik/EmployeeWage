f6d5be45acffc9ca28523b3ff14f33776caef94e
TalatiHardik <hardikhtalati@gmail.com>
2020-07-13 11:56:01 +0530

    UC5 done

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,20 +1,26 @@
 
 #!/bin/bash -x
 
-echo "Welcome to UC4 Add case statement"
+echo "Welcome to UC5 calculate wage for month"
 isfulltime=1
 isparttime=2
-empcheck=$(( RANDOM%3 ))
+totalSalary=0
+numWorkingDays=20
+
 empRatePerHr=20
+for((day=1;day<=$numWorkingDays;day++))
+do
+	empcheck=$(( RANDOM%3 ))
+	case $empcheck in
+		$isparttime)
+		emphrs=4 ;;
 
-case $empcheck in
-	$isparttime)
-	emphrs=4 ;;
-	$isfulltime)
-	emphrs=8 ;;
-	*)
-	emphrs=0 ;;
-esac
-salary=$(( emphrs*empRatePerHr))
-
-echo $salary "is salary of employee "
+		$isfulltime)
+		emphrs=8 ;;
+		*)
+		emphrs=0 ;;
+	esac
+	salary=$(( $emphrs*$empRatePerHr ))
+	totalSalary=$(($totalSalary+$salary))
+done
+echo $totalSalary "is salary of employee "

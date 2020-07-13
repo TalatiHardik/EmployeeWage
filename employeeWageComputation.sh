a4febe096137e4dad9a21386a60ec934fa4cd9ff
TalatiHardik <hardikhtalati@gmail.com>
2020-07-13 12:14:23 +0530

    UC7 completed

--- employeeWageComputation.sh
+++ employeeWageComputation.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash -x
-echo "Welcome to UC6 - Add another condition of max hours"
+echo "Welcome to UC7 - Add functions"
+
 Is_Fulltime=1
 Is_Parttime=2
 Max_Hrs_IN_Month=100
@@ -12,20 +13,24 @@ totalSalary=0
 totalEmpHr=0
 totalWorkingDays=0
 
+function getWorkingHours(){
+	case $1 in
+		$Is_Fulltime)
+		workHours=4 ;;
+		$Is_Parttime)
+		workHours=8 ;;
+		*)
+		workHours=0 ;;
+	esac
+	echo $workHours
+}
+
 
 while [[ $totalEmpHr -lt $Max_Hrs_IN_Month && $totalWorkingDays -lt $Num_Working_Days ]]
 do
 	((totalWorkingDays++))
-	empcheck=$(( RANDOM%3 ))
-	case $empcheck in
-		$Is_Fulltime)
-		emphrs=4 ;;
-		$Is_Parttime)
-		emphrs=8 ;;
-		*)
-		emphrs=0 ;;
-	esac
-	totalEmpHr=$(($totalEmpHr+$emphrs))
+	workHours="$( getWorkingHours $((RANDOM%3)) )"
+	totalEmpHr=$(($totalEmpHr+$workHours))
 done
 totalSalary=$(($totalEmpHr*$Emp_Rate_Per_Hr))
 
